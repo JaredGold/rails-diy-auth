6a6758674fbc29ce5f03a8736b1946cff8fd70f5
JaredGold <jegoldstein94@gmail.com>
2021-02-01 16:30:06 +1100

    Finish auth vid 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   root 'pages#home'
-  
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/signin', to: 'sessions#create', as: 'create_session'
+  get '/signout', to: 'sessions#destroy', as: 'signout'
 end
